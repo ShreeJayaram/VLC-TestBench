d179cd9bdb41bf405d4a9fc11a1e2c2abb1e99f7
ShreeJayaram <shreej@bu.edu>
2017-07-24 14:53:57 -0400

    Add files via upload

---
+++ makezero.m
@@ -0,0 +1,107 @@
+
+[time0, volt0] = importAgilentBin('dcr_0.bin'); 
+subplot(4,4,1);
+plot(time0,volt0)
+title('Graph 0.500')
+
+
+[time1, volt1] = importAgilentBin('dcr_1.bin'); 
+subplot(4,4,2);
+plot(time1,volt1)
+title('Graph 0.600')
+
+ 
+[time2, volt2] = importAgilentBin('dcr_2.bin'); 
+subplot(4,4,3);
+plot(time2,volt2)
+title('Graph 0.700')
+
+
+[time3, volt3] = importAgilentBin('dcr_3.bin'); 
+subplot(4,4,4);
+plot(time3,volt3)
+title('Graph 0.800')
+
+
+[time4, volt4] = importAgilentBin('dcr_4.bin'); 
+subplot(4,4,5);
+plot(time4,volt4)
+title('Graph 0.900')
+
+
+[time5, volt5] = importAgilentBin('dcr_5.bin'); 
+subplot(4,4,6);
+plot(time5,volt5)
+title('Graph 0.1000')
+
+
+[time6, volt6] = importAgilentBin('dcr_6.bin'); 
+subplot(4,4,7);
+plot(time6,volt6)
+title('Graph 0.1100')
+
+
+[time7, volt7] = importAgilentBin('dcr_7.bin'); 
+subplot(4,4,8);
+plot(time7,volt7)
+title('Graph 0.1200')
+
+
+[time8, volt8] = importAgilentBin('dcr_8.bin'); 
+subplot(4,4,9);
+plot(time8,volt8)
+title('Graph 0.1300')
+
+
+[time9, volt9] = importAgilentBin('dcr_9.bin'); 
+subplot(4,4,10);
+plot(time9,volt9)
+title('Graph 0.1400')
+
+
+[time10, volt10] = importAgilentBin('dcr_10.bin'); 
+subplot(4,4,11);
+plot(time10,volt10)
+title('Graph 0.1500')
+
+
+[time11, volt11] = importAgilentBin('dcr_11.bin'); 
+subplot(4,4,12);
+plot(time11,volt11)
+title('Graph 0.1600')
+
+
+[time12, volt12] = importAgilentBin('dcr_12.bin'); 
+subplot(4,4,13);
+plot(time12,volt12)
+title('Graph 0.1700')
+
+
+[time13, volt13] = importAgilentBin('dcr_13.bin'); 
+subplot(4,4,14);
+plot(time13,volt13)
+title('Graph 0.1800')
+
+
+[time14, volt14] = importAgilentBin('dcr_14.bin'); 
+subplot(4,4,15);
+plot(time14,volt14)
+title('Graph 0.1900')
+
+
+[time14, volt15] = importAgilentBin('dcr_15.bin'); 
+subplot(4,4,16);
+plot(time14,volt14)
+title('Graph 0.2000')
+
+
+
+
+
+
+
+
+
+
+
+
