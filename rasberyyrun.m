907255784c6ba1d60def3d1ab5532bac461f62da
Shreenidhi Jayaram <28953941+ShreeJayaram@users.noreply.github.com>
2017-07-25 12:54:31 -0400

    Add files via upload

---
+++ rasberyyrun.m
@@ -0,0 +1,103 @@
+
+f = fopen('dcrtest3_1.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,1);
+plot(v); 
+
+f = fopen('dcrtest3_2.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,2);
+plot(v); 
+
+f = fopen('dcrtest3_4.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,3);
+plot(v); 
+
+f = fopen('dcrtest3_4.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,4);
+plot(v); 
+
+f = fopen('dcrtest3_5.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,5);
+plot(v); 
+
+f = fopen('dcrtest3_6.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,6);
+plot(v); 
+
+f = fopen('dcrtest3_7.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,7);
+plot(v); 
+
+f = fopen('dcrtest3_8.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,8);
+plot(v); 
+
+f = fopen('dcrtest3_9.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,9);
+plot(v); 
+
+f = fopen('dcrtest3_10.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,10);
+plot(v); 
+
+f = fopen('dcrtest3_11.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,11);
+plot(v); 
+
+f = fopen('dcrtest3_12.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,12);
+plot(v); 
+
+f = fopen('dcrtest3_13.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,13);
+plot(v); 
+
+f = fopen('dcrtest3_14.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,14);
+plot(v); 
+
+f = fopen('dcrtest3_15.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,15);
+plot(v); 
+
+f = fopen('dcrtest3_16.bin', 'rb');
+v = fread(f,400,'float');
+fclose(f);
+subplot(4,4,16);
+plot(v); 
+
+
+
+
+
+
+
